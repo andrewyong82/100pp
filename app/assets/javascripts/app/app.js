c198c4d3cf1c70c85769a5c0c5fb40f14539b651
andrewyong82 <andrewyong82@gmail.com>
2016-10-18 18:18:17 +0800

    big give alert

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -18,7 +18,7 @@ var App = window.App = Skull.View.extend({
             $('.main-header, .hero-search').addClass('with-fixed-alert');
             this.fixedAlert = true;
         }
-        if(!window.store.get('globalClosedCookies')){
+        if(!window.store.get('globalClosedCookies2')){
             $('#global-alert').slideDown(400);
             $('.main-header').addClass('with-global-alert');
             if(this.fixedAlert){
@@ -31,7 +31,7 @@ var App = window.App = Skull.View.extend({
     closeAlert: function(event){
         $('#global-alert').slideUp(400);
         $('.main-header').removeClass('with-global-alert').removeClass('with-two-alerts');
-        window.store.set('globalClosedCookies', true);
+        window.store.set('globalClosedCookies2', true);
         this.globalAlert = false;
     },
 
